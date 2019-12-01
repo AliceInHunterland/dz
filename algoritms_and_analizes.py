ae8ffceb9077d5f5ac3d2bc06cb52afb0bf57b0a
AliceInHunterland <35739873+AliceInHunterland@users.noreply.github.com>
2019-12-02 01:24:05 +0300

    Update algoritms_and_analizes.py

--- algoritms_and_analizes.py
+++ algoritms_and_analizes.py
@@ -18,27 +18,24 @@ def generator(size):
 
 def npgauss(A,b):
     return(np.linalg.solve(A,b))
-def gauss(A,b):
 
+
+def gauss(A,b):
     Ab = np.hstack([A, b.reshape(-1, 1)])
     n = len(b)
     for i in range(n):
         a = Ab[i]
-
         for j in range(i + 1, n):
             b = Ab[j]
             m = a[i] / b[i]
             Ab[j] = a - m * b
-
     for i in range(n - 1, -1, -1):
         Ab[i] = Ab[i] / Ab[i, i]
         a = Ab[i]
-
         for j in range(i - 1, -1, -1):
             b = Ab[j]
             m = a[i] / b[i]
             Ab[j] = a - m * b
-
     x = Ab[:, 3]
    # return(x)
  
